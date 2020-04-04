9db1d962c707f7f66b72996a968daf41deafa43d
EijiGorilla <emasuz@hotmail.com>
2020-04-04 12:43:35 +0800

    web app updated commit

--- WebApp/ConfigurableApp/TimeAware/config/defaults.js
+++ WebApp/ConfigurableApp/TimeAware/config/defaults.js
@@ -19,7 +19,7 @@ define({
   //Default configuration settings for the application. This is where you'll define things like a bing maps key,
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
   "appid": "",
-  "webmap": "6d5476675305488e8317de3ac8cd6982",
+  "webmap": "cb62cd213b9d4755b7e91b34dfb51cf7",
   "oauthappid": null, //"AFTKRmv16wj14N3z",
   //Group templates must support a group url parameter. This will contain the id of the group.
   "group": "",
