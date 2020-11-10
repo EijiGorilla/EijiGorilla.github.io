d0ecd86a05f8b0a29d51f458582d9f26e0a313b5
EijiGorilla <emasuz@hotmail.com>
2020-11-10 12:32:33 +0900

    update

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -5,6 +5,24 @@ myImage.onclick = function() {
     if(mySrc === 'https://EijiGorilla.github.io/web-projects/test-site/images/fire-fox.jpg') {
         myImage.setAttribute('src','https://EijiGorilla.github.io/web-projects/test-site/images/firefox2.jpg');
     } else {
-        myImage.setAttribute('src','https://EijiGorilla.github.io/web-projects/test-site/images/fire-fox.jpg')
+        myImage.setAttribute('src','https://EijiGorilla.github.io/web-projects/test-site/images/fire-fox.jpg');
     }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name:');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+
+myButton.onclick = function() {
+    setUserName();
 }
