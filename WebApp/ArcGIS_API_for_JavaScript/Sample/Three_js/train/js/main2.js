36433a32eea2637c9b72f18f8a8005710a5dd3d0
EijiGorilla <emasuz@hotmail.com>
2022-10-07 16:15:24 +0800

    updated

--- WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/train/js/main2.js
+++ WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/train/js/main2.js
@@ -260,7 +260,7 @@ require([
           iss: null, 
           iss2: null,                                                   // ISS model
           issScale: 3,                                     // scale for the iss model
-          issScale2: 1000,
+          issScale2: 1,
           path: null,
         count: null,
         up: null,
@@ -372,7 +372,7 @@ require([
               });
 
               // 2nd Object
-              var issMeshUrl2 = "https://EijiGorilla.github.io/WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/3d-model-gltf/assets/Running1.glb"; 
+              var issMeshUrl2 = "https://EijiGorilla.github.io/WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/3d-model-gltf/assets/Waterwheel.glb"; 
               //"https://EijiGorilla.github.io/WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/3d-model-gltf/assets/Waterwheel.glb"
               //"https://EijiGorilla.github.io/WebApp/ArcGIS_API_for_JavaScript/Sample/Three_js/3d-model-gltf/assets/Car4ReScaled.glb"
               loaderGLTF.load(issMeshUrl2, function(gltf) {
